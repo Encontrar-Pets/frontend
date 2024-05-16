0b630310c41d506afdc073046d8a26420bab808b
Felipe P. Fuzaro <felipefuzaro@hotmail.com>
2024-05-16 22:26:52 +0200

    feat: add playbook

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -8,7 +8,7 @@ type InputProps = {
 export default function Input(props: InputProps) {
   return (
     <input
-      className={`border-solid border-[1px] border-[#BFC4D7] text-[#71747B] rounded-md py-2 px-4 focus:outline-none ${props.className}`}
+      className={`border-solid border-[1px] border-[#BFC4D7] text-[#71747B] rounded-md py-2 px-4 focus:outline-none ${props.className} mx-6 my-2`}
       placeholder={props.placeholder}
       value={props.value}
       onChange={(e) => props.onChange(e.target.value)}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from "./pages/home";
+import Playbook from './pages/playbook';
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/playbook' element={<Playbook />} />
       </Routes>
     </Router>
   )
