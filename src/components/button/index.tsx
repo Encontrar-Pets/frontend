0c16e7569a5ac26390a06ee5db71ea8de7f8aa57
Guilherme Couto <guicoutof@hotmail.com>
2024-05-16 19:13:22 -0300

    feat: button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,8 @@
 type ButtonProps = {
   label: string;
   onClick: () => void;
-  type?: "filled" | "outline";
+  className?: string;
+  type?: "filled" | "outline" | "text";
   variant?: "primary" | "secondary" | "tertiary";
 };
 
@@ -10,38 +11,53 @@ export default function Button({
   onClick,
   variant = "primary",
   type = "filled",
+  className,
 }: ButtonProps) {
-  const isFilled = () => type === "filled";
-
   const bgVariants = {
-    primary: 'bg-primary',
-    secondary: 'bg-secondary',
-    tertiary: 'bg-tertiary',
-  }
+    primary: "bg-primary",
+    secondary: "bg-secondary",
+    tertiary: "bg-tertiary",
+  };
 
   const borderVariants = {
-    primary: 'border-primary',
-    secondary: 'border-secondary',
-    tertiary: 'border-tertiary',
+    primary: "border-primary",
+    secondary: "border-secondary",
+    tertiary: "border-tertiary",
+  };
+
+  function getTextButtonClassName() {
+    return `rounded p-[14px] border-2 border-white hover:opacity-80 hover:border-primary-gray  ${className}`;
   }
 
-  if (isFilled()) {
-    return (
-      <button
-        className={`my-2 mx-6 rounded p-4 ${bgVariants[variant]}`}
-        onClick={onClick}
-      >
-        <span className={`text-sm font-bold text-white`}>{label}</span>
-      </button>
-    );
+  function getOutlineClassName() {
+    return `h-[60px] border-2 ${borderVariants[variant]} rounded p-[14px] bg-white hover:opacity-70 ${className}`;
   }
 
-  return (
-    <button
-      className={`my-2 mx-6 border-2 ${borderVariants[variant]} rounded p-[14px] bg-white`}
-      onClick={onClick}
-    >
-      <span className={`text-sm font-bold text-black`}>{label}</span>
-    </button>
-  );
+  function getFilledClassName() {
+    return `h-[60px] rounded p-4 ${bgVariants[variant]} hover:opacity-80 ${className}`;
+  }
+
+  switch (type) {
+    case "text":
+      return (
+        <button className={getTextButtonClassName()} onClick={onClick}>
+          <span className={`text-sm font-bold text-primary-gray`}>{label}</span>
+        </button>
+      );
+
+    case "outline":
+      return (
+        <button className={getOutlineClassName()} onClick={onClick}>
+          <span className={`text-sm font-bold text-black`}>{label}</span>
+        </button>
+      );
+
+    case "filled":
+    default:
+      return (
+        <button className={getFilledClassName()} onClick={onClick}>
+          <span className={`text-sm font-bold text-white`}>{label}</span>
+        </button>
+      );
+  }
 }
