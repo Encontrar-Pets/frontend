1ec5db83618ae843c4a309163ee6eb2f46a971f0
Bruno Cardoso <bruno.soarescardoso@outlook.com>
2024-05-15 22:53:04 -0300

    initial setup

---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex bg-black sm:bg-red md:bg-grey-400">
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Home Example
+      </h1>
+    </div>
+  );
+}
+
