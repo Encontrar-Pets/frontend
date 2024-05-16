2f32df71ab0a3ce600fa7311d18dbf597f73bd20
Bruno Cardoso <bruno.soarescardoso@outlook.com>
2024-05-16 10:03:11 -0300

    add tailwind screens sizes and colors

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
+import Logo from 'assets/images/rs_flag.png';
+
 export default function Home() {
   return (
-    <div className="flex bg-black sm:bg-red md:bg-grey-400">
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Home Example
-      </h1>
+    <div className='flex w-full justify-center'>
+      <div className="flex max-w-96">
+        <img src={Logo} alt="logo" />
+      </div>
     </div>
   );
 }
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,26 @@ module.exports = {
   theme: {
     screens: {
       'sm': { 'min': '640px', 'max': '767px' },
-      // => @media (min-width: 640px and max-width: 767px) { ... }
-
       'md': { 'min': '768px', 'max': '1023px' },
-      // => @media (min-width: 768px and max-width: 1023px) { ... }
-
       'lg': { 'min': '1024px', 'max': '1279px' },
-      // => @media (min-width: 1024px and max-width: 1279px) { ... }
-
       'xl': { 'min': '1280px', 'max': '1535px' },
-      // => @media (min-width: 1280px and max-width: 1535px) { ... }
-
       '2xl': { 'min': '1536px' },
-      // => @media (min-width: 1536px) { ... }
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#60815C',
+        secondary: '#F30E0E',
+        'primary-gray': '#757575',
+        'secondary-gray': '#E0E0E0',
+        'tertiary-gray': '#F5F5F5',
+        'primary-red': '#D16767',
+        'secondary-red': '#D1676780',
+        'primary-yellow': '#FFCF6F',
+        'secondary-yellow': '#F3EA0E',
+        'primary-green': '#6CB28E',
+        'secondary-green': '#61E597',
+      },
+    },
   },
   plugins: [],
 }
