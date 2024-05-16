5a6bce3401214cd0ceec5675ce94c34c51124387
Bruno Cardoso <bruno.soarescardoso@outlook.com>
2024-05-16 20:06:49 -0300

    add animal component

---
+++ src/components/animalcard/index.tsx
@@ -0,0 +1,19 @@
+type AnimalCardProps = {
+  title: string;
+  imageUrl: string;
+  description: string;
+  onClick: () => void;
+};
+
+export default function AnimalCard({ title, description, imageUrl, onClick }: AnimalCardProps) {
+  return (
+    <button onClick={onClick}>
+      <div className="flex flex-col  max-w-sm rounded-lg overflow-hidden shadow-lg bg-white">
+        <img className="rounded-t-lg h-44" src={imageUrl} alt={title} />
+
+        <h1 className="self-start ml-2 mt-1 text-lg font-semibold">{title}</h1>
+        <span className="self-start ml-2 text-sm mb-2 text-primary-gray">{description}</span>
+      </div>
+    </button>
+  )
+}
--- src/pages/petlist/index.tsx
+++ src/pages/petlist/index.tsx
@@ -1,3 +1,5 @@
+import AnimalCard from "components/animalcard";
+
 export default function PetList() {
   return (
     <div className='flex w-full justify-center px-4'>
@@ -5,6 +7,12 @@ export default function PetList() {
 
         <b className='mt-6 text-gray-700'>Caracteristicas</b>
 
+        <AnimalCard
+          imageUrl="https://i0.statig.com.br/bancodeimagens/78/pt/gs/78ptgsfeddfh638dkkzya5p3y.jpg"
+          onClick={() => window.location.href = '/pet/1'}
+          title="Caramelo Médio"
+          description="Galpão 1 ULBRA - Corredor 4 - Baia 112"
+        />
       </div>
     </div>
   );
