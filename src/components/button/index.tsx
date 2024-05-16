9e61f99789282fff438cb765afa0783d7ec4e3d3
Felipe P. Fuzaro <felipefuzaro@hotmail.com>
2024-05-16 22:11:11 +0200

    fix: dynamic colors in button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -13,10 +13,22 @@ export default function Button({
 }: ButtonProps) {
   const isFilled = () => type === "filled";
 
+  const bgVariants = {
+    primary: 'bg-primary',
+    secondary: 'bg-secondary',
+    tertiary: 'bg-tertiary',
+  }
+
+  const borderVariants = {
+    primary: 'border-primary',
+    secondary: 'border-secondary',
+    tertiary: 'border-tertiary',
+  }
+
   if (isFilled()) {
     return (
       <button
-        className={`my-2 mx-6 rounded p-4 bg-${variant}`}
+        className={`my-2 mx-6 rounded p-4 ${bgVariants[variant]}`}
         onClick={onClick}
       >
         <span className={`text-sm font-bold text-white`}>{label}</span>
@@ -26,7 +38,7 @@ export default function Button({
 
   return (
     <button
-      className={`my-2 mx-6 border-2 border-${variant} rounded p-[14px] bg-white`}
+      className={`my-2 mx-6 border-2 ${borderVariants[variant]} rounded p-[14px] bg-white`}
       onClick={onClick}
     >
       <span className={`text-sm font-bold text-black`}>{label}</span>
