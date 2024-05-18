75356707e755df4eaeaa0a3490da8ae6387389ee
Guilherme Couto <guicoutof@hotmail.com>
2024-05-17 23:39:55 -0300

    feat: upload image

--- src/components/image-upload/image-upload.tsx
+++ src/components/image-upload/image-upload.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useRef, useState } from "react";
+import imageCompression from "browser-image-compression";
 import Plus from "assets/icons/plus.svg";
 
 type ImageUploadProps = {
   className?: string;
+  onChange: (baseImage: string) => void;
 };
 
 export default function ImageUpload(props: ImageUploadProps) {
   const [image, setImage] = useState<File | null>(null);
   const [preview, setPreview] = useState("");
+  const [error, setError] = useState(true);
 
   useEffect(() => {
     if (!image) {
@@ -23,14 +26,41 @@ export default function ImageUpload(props: ImageUploadProps) {
 
   const hiddenFileInput = useRef<any>(null);
 
-  const handleClick = () => {
-    if (hiddenFileInput.current) hiddenFileInput.current.click();
+  const getBase64 = (file: File): Promise<string | ArrayBuffer | null> => {
+    return new Promise((resolve) => {
+      let reader = new FileReader();
+
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+    });
   };
 
-  const handleChange = (file: File | null) => {
-    if (file) {
-      setImage(file);
+  const handleChange = async (file: File | null) => {
+    try {
+      if (!file) throw new Error("No file found");
+
+      const compressedFile = await imageCompression(file, {
+        maxSizeMB: 1,
+        maxWidthOrHeight: 1920,
+      });
+
+      const baseImage = await getBase64(compressedFile);
+
+      if (!baseImage) throw new Error("Error on get base64 image");
+
+      props.onChange(baseImage.toString());
+      setImage(compressedFile);
+    } catch (error) {
+      setError(true);
     }
+  };
+
+  const handleClick = () => {
+    setError(false);
+    if (hiddenFileInput.current) hiddenFileInput.current.click();
   };
 
   return (
@@ -40,21 +70,28 @@ export default function ImageUpload(props: ImageUploadProps) {
         onClick={handleClick}
       >
         {image ? (
-          <img
-            className="rounded-[34px] self-center"
-            src={preview}
-            alt="preview"
-            height="160px"
-          />
+          <div className="h-[160px]">
+            <img
+              className="rounded-[34px] self-center"
+              src={preview}
+              alt="preview"
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
         ) : (
           <div className="flex flex-col justify-center items-center">
             <span className="mb-6 text-primary-gray font-xl font-bold">
               Adicionar foto
             </span>
-            <img src={Plus} alt="add" height={160} />
+            <img src={Plus} alt="add" />
           </div>
         )}
       </button>
+      {error && (
+        <span className="ml-4 mt-2 text-primary-red text-xs">
+          Erro ao carregar a foto. Tente novamente ou escolha outra imagem!
+        </span>
+      )}
       <input
         type="file"
         accept="image/*"
